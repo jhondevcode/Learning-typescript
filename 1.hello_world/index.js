1502040412b3f1c791a04e6756c94acdde51cdd0
jhondev-code <jhondev.code@gmail.com>
2021-05-10 11:24:13 -0500

    Basic hello world in typescript

---
+++ 1.hello_world/index.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * ===========================================================================
+ * Name        : 1.hello_world
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : Basic hello world in typescript
+ * Created on  : 2021-05-10 11:22:12.747846
+ * ===========================================================================
+ */
+console.log("Hello World!");
